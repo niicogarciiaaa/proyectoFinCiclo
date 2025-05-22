df94ee8d466ab2233aef3da8be157190e29ce2e1
Nicolás García Moreira <nicolas-garcia@tecdesoft.es>
2025-05-22 14:36:35 +0200

    se arregla el problema de CORS, y se actualizan los wireframes, y la memoria hasta el punto del presupusto

--- PHP/config/cors.php
+++ PHP/config/cors.php
@@ -1,9 +1,9 @@
 <?php
 function configureCors() {
-    header("Access-Control-Allow-Origin: http://localhost:4200");
+    header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Credentials: true");
     header("Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS");
-    header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
+    header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With, content-type");
     header("Content-Type: application/json; charset=UTF-8");
 
     // Manejar solicitudes preflight OPTIONS
